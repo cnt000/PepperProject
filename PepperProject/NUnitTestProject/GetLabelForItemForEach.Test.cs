2250ccf05ca820397146dbda8ff8ef4cc78290be
Edoardo Gargano <edoardo.gargano@yoox.com>
2019-08-12 15:30:47 +0200

    feat: empty test

--- PepperProject/NUnitTestProject/GetLabelForItemForEach.Test.cs
+++ PepperProject/NUnitTestProject/GetLabelForItemForEach.Test.cs
@@ -480,5 +480,43 @@ namespace Tests
             StringAssert.AreEqualIgnoringCase(label, "x/y/z.json",
                 "ThreeOccurrencesSecondTupleOnCode10AndMicroAndMacroWinMacro");
         }
+
+        [Test]
+        public void Empty()
+        {
+            var configuration = new Dictionary<string, Dictionary<string[], string[]>>
+            {
+                {
+                    "code10", new Dictionary<string[], string[]>
+                    {
+                        {new[] {"100", "300", "500"}, new[] {"a", "b", "d.json"}},
+                        {new[] {"1", "44444", "50"}, new[] {"x", "y", "z.json"}}
+                    }
+                },
+                {
+                    "micro", new Dictionary<string[], string[]>
+                    {
+                        {new[] {"1", "30", "50"}, new[] {"a", "b", "f.json"}},
+                        {new[] {"55555", "40", "60"}, new[] {"x", "y", "j.json"}}
+                    }
+                },
+                {
+                    "macro", new Dictionary<string[], string[]>
+                    {
+                        {new[] {"1", "15", "15"}, new[] {"a", "b", "readytowear.json"}},
+                        {new[] {"2", "4", "66666"}, new[] {"x", "y", "z.json"}}
+                    }
+                }
+            };
+            var ids = new Dictionary<string, string>
+            {
+                {"code10", "12345"},
+                {"micro", "67890"},
+                {"macro", "09876"}
+            };
+            var label = GetLabelForDimensions.GetLabelForItemForEach(ids, configuration);
+            StringAssert.AreEqualIgnoringCase(label, "",
+                "Empty");
+        }
     }
 }
